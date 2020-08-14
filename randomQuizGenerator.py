bb72153e7233ecc531011705ba1dd4ded1f8497b
john doe <scottbruceburton@gmail.com>
2020-08-14 15:25:08 -0400

    started .py with dict

---
+++ randomQuizGenerator.py
@@ -0,0 +1,28 @@
+#! python3
+# Create 35 different Quizzes
+# Create 50 multiple-choice questions for each quiz in random order
+# Provide the correct answer and three random wrong answers in random order
+# Write the quizzes to 35 text files
+# write the answer keys to 35 text files
+
+import random
+#the Quiz Data. Keys are States and values are their capitals
+capitals = {'Alabama': 'Montgomery', 'Alaska': 'Juneau', 'Arizona': 'Phoenix',
+        'Arkansas': 'Little Rock', 'California': 'Sacramento', 'Colorado':
+        'Denver', 'Connecticut': 'Hartford', 'Delaware': 'Dover', 'Florida':
+        'Tallahassee', 'Georgia': 'Atlanta', 'Hawaii': 'Honolulu', 'Idaho':
+        'Boise', 'Illinois': 'Springfield', 'Indiana': 'Indianapolis', 'Iowa':
+        'Des Moines', 'Kansas': 'Topeka', 'Kentucky': 'Frankfort', 'Louisiana':
+        'Baton Rouge', 'Maine': 'Augusta', 'Maryland': 'Annapolis',
+        'Massachusetts': 'Boston', 'Michigan': 'Lansing', 'Minnesota': 'Saint
+        Paul', 'Mississippi': 'Jackson', 'Missouri': 'Jefferson City',
+        'Montana': 'Helena', 'Nebraska': 'Lincoln', 'Nevada': 'Carson City',
+        'New Hampshire': 'Concord', 'New Jersey': 'Trenton', 'New Mexico':
+        'Santa Fe', 'New York': 'Albany', 'North Carolina': 'Raleigh', 'North
+        Dakota': 'Bismarck', 'Ohio': 'Columbus', 'Oklahoma': 'Oklahoma City',
+        'Oregon': 'Salem', 'Pennsylvania': 'Harrisburg', 'Rhode Island':
+        'Providence', 'South Carolina': 'Columbia', 'South Dakota': 'Pierre',
+        'Tennessee': 'Nashville', 'Texas': 'Austin', 'Utah': 'Salt Lake City',
+        'Vermont': 'Montpelier', 'Virginia': 'Richmond', 'Washington':
+        'Olympia', 'West Virginia': 'Charleston', 'Wisconsin': 'Madison',
+        'Wyoming': 'Cheyenne'}
